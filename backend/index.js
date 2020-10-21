75fadb6d3661a08f23e40e6fc41018231f220ec4
TS000 <tylersnell@me.com>
2020-10-21 14:46:27 -0600

    testing Handlers

--- backend/index.js
+++ backend/index.js
@@ -2,48 +2,63 @@ var express = require('express');
 var cors = require('cors')
 var app = express();
 
-
-
-// SERVER
-app.use(cors());
-
-app.listen(3030, () => {
- console.log("Server running on port http://localhost:3030");
-});
-
-app.get("/url", (req, res, next) => {
-    res.json(["Red","Lime","Magenta","Green","Fuchsia"]);
-   });
-
-
 // Sentry
 const Sentry = require("@sentry/node");
-// or use es6 import statements
-// import * as Sentry from '@sentry/node';
 
-const Tracing = require("@sentry/tracing");
-// or use es6 import statements
-// import * as Tracing from '@sentry/tracing';
+// const Tracing = require("@sentry/tracing");
 
 Sentry.init({
-  dsn: "https://9a451517d47d43b4a726674aaf3c1b9a@o463979.ingest.sentry.io/5469713",
+  dsn: "https://6142479db5c04c51a0fc707abca44bfe@o463979.ingest.sentry.io/5469698",
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
 });
 
-const transaction = Sentry.startTransaction({
-  op: "test",
-  name: "My First Test Transaction",
-});
+app.use(Sentry.Handlers.requestHandler());
 
-setTimeout(() => {
-  try {
-    foo();
-  } catch (e) {
-    Sentry.captureException(e);
-  } finally {
+const transaction = Sentry.startTransaction({
+    op: "test correlative error features: thumbprint",
+    name: "My First Test Transaction",
+  });
+
+// SERVER
+app.use(cors());
+
+app.get("/", function rootHandler(req, res) {
+    res.end("Hello world!");
+  });
+
+  app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("Request failed");
+  });
+
+app.get("/url", (req, res, next) => {
+    // res.json(["Red","Lime","Magenta","Green","Fuchsia"]);
+    res.status(500).send('Something broke!')
+    Sentry.captureException();
     transaction.finish();
-  }
-}, 99);
+   });
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+  });
+
+  app.listen(3030, () => {
+    console.log("Server running on port http://localhost:3030");
+   });
+
+// setTimeout(() => {
+//   try {
+//     console.log('foo')
+//   } catch (e) {
+//     Sentry.captureException(e);
+//   } finally {
+//     transaction.finish();
+//   }
+// }, 99);
