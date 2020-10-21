e4c735a8bbc08894baa2fc4472294deb1eafc626
TS000 <tylersnell@me.com>
2020-10-21 14:47:03 -0600

    added debug url to FE fetch call

--- src/fakeRequest.js
+++ src/fakeRequest.js
@@ -1,17 +1,19 @@
-const fakeArr = {
-    description: "bug",
-    email: "e@d.com",
-    firstName: "timmy",
-    lastName: "tammy",
-    oAuthApplicationType: "stealth api",
-    oAuthRedirectURI: "https://redirecterersd.com",
-    organization: "Cool Co."
-  };
+// const fakeArr = {
+//     description: "bug",
+//     email: "e@d.com",
+//     firstName: "timmy",
+//     lastName: "tammy",
+//     oAuthApplicationType: "stealth api",
+//     oAuthRedirectURI: "https://redirecterersd.com",
+//     organization: "Cool Co."
+//   };
 
-  const url = "http://localhost:3030/url";
+  export const url = "http://localhost:3030/url";
+
+  export const debugURL = "http://localhost:3030/debug-sentry"
 
   export const request = new Request(url, {
-    body: JSON.stringify(fakeArr),
+    body: "a=1",
     headers: {
       accept: "application/json",
       "content-type": "application/json"
