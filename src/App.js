7e17e5679599b7960481173cce66ea2555a530e0
TS000 <tylersnell@me.com>
2020-10-21 14:45:38 -0600

    error boundry

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
-import './App.css';
-import { request } from './fakeRequest'
+import React, { useState } from "react";
+import "./App.css";
+import { debugURL } from "./fakeRequest";
+import * as Sentry from "@sentry/react";
+
+const FallbackComponent = () => {
+  return <div>An error has occured, panic.</div>;
+};
 
 const App = () => {
-  const [ newData, setData ] = useState('no data');
+  const [newData, setData] = useState("no data");
 
   const fetcher = () => {
-  fetch(request)
-    .then((response) => {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-      }
-      response.json().then(function(data) {
-        console.log(data);
-        setData(data)
+    fetch(debugURL)
+      .then((response) => {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          Sentry.captureException(response.status);
+          return;
+        }
+        response.json().then(function (data) {
+          console.log(data);
+          setData(data);
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
       });
-    })
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-  }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => fetcher()} style={{padding: "40, 100"}}>Break the world</button>
-        <h4>Data: {newData}</h4>
-      </header>
-    </div>
+    <Sentry.ErrorBoundary fallback={FallbackComponent} showDialog>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => fetcher()} style={{ padding: "40, 100" }}>
+            Break the world
+          </button>
+          <h4>Data: {newData}</h4>
+        </header>
+      </div>
+    </Sentry.ErrorBoundary>
   );
-}
+};
 
 export default App;
